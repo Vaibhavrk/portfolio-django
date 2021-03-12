466fbb547f35a007bf1074b23e048373c5d51778
Vaibhavrk <khandekarv0@gmail.com>
2021-03-12 23:23:29 +0530

    made changes in frontend

--- myportfolio/settings.py
+++ myportfolio/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
 from pathlib import Path
-import os
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -124,5 +124,5 @@ STATIC_URL = '/staticfiles/'
 MEDIA_URL = '/images/'
 
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR,'staticfiles')
+    BASE_DIR / "staticfiles",
 ]
